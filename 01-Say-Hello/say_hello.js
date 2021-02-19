30cbf78192b33540f038afe05abb44b8c2344830
Souad Allahham <souadallahham89@gmail.com>
2021-02-19 12:22:36 +0100

    I finished Ex 1 for lesson 12

--- 01-Say-Hello/say_hello.js
+++ 01-Say-Hello/say_hello.js
@@ -1,8 +1,17 @@
 //insert your pseudocode below
 /*
-
-here
+- create a variable represents your name.
+- prompt with a message says "Please Insert Your Name"
+- creat a variabel (constant) for the greeting (Hello).
+- print out the greeting using your name.
 
 */
 
 //insert your code below
+
+var username, Greeting;
+
+
+username = window.prompt('Please Insert Your Username');
+Greeting = ' Hello  ';
+window.alert(Greeting + username);
