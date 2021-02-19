71cf4b8996c4dcac9bdac866a6104cafe2539c75
Souad Allahham <souadallahham89@gmail.com>
2021-02-19 14:11:18 +0100

    I finished EX 2 for lesson 12

--- 02-Counting-Characters/counting_characters.js
+++ 02-Counting-Characters/counting_characters.js
@@ -1,8 +1,19 @@
 //insert your pseudocode below
 /*
+- create a variable represent an input.
+- determine the type of the variable (string).
+- prompt this variable with a certain message.
+- count the characters of the input (length).
+- print out a result.
 
-here
+Note: 1-mistake by user could be to insert a numbere instead of letter.
+      2- we should contrain the user to enter just letters without any special characters.
 
 */
 
 //insert your code below
+var name2;
+
+
+name2 = window.prompt('Please Insert your Name');
+window.alert(name2.length);
